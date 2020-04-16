6cde3a59f54584223cca454be25e26ad62ac5b60
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2020-04-16 18:44:36 +0000

    Commiting

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"  # you should make this more random and unique
+app.config['SECRET_KEY'] = "A0Zr98j/3yX R~XHH!jmN]LWX/,?RT"
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:@Poogiebear@localhost/project"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
+app.config['SQLALCHEMY_DATABASE_URL'] = "postgres://gojtqlllhhakzr:f25325e71b9e5c31926ab00a7d9f97c3a37041deb26fc878298b0b6f36b80907@ec2-54-157-78-113.compute-1.amazonaws.com:5432/dbueclfqep5ei"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  #suppress a warning
 
 db = SQLAlchemy(app)
 
